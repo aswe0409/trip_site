305e278be36f78c3b89ad116e911a0f0e768ec25
SeokYeong Jeong <aswe0409@gmail.com>
2024-05-23 10:37:49 +0900

    Revert "feat: select festival range list & select festival detail"
    This reverts commit 80505f8824ab6be5fe263410e09b3a6521896050.

--- backend/src/main/java/com/example/trip/festival/service/festivalService.java
+++ backend/src/main/java/com/example/trip/festival/service/festivalService.java
@@ -17,15 +17,4 @@ public class festivalService {
 	public List<Map<String, Object>> showList(){
 		return fdao.showList();
 	}
-	
-	
-	//축제 범위 값 보내주기
-	public List<Map<String, Object>> rangeList(int startIdx, int endIdx){
-		return fdao.rangeList(startIdx, endIdx);
-	}
-	
-	//축제 상세보기(하나 값 전달)
-	public List<Map<String, Object>> showDetail(int no){
-		return fdao.showDetail(no);
-	}
 }
